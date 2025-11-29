b6bd3b722efe6f94c18fb76e47bce58b4afe1a9e
adammrock <adamrock0109@gmail.com>
2025-11-29 14:37:18 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+/*
+	Adam Rock
+	File Name: script.js
+	Date: 11/11/2025
+*/
+
+//Hamburger menu
+function menu() {
+	var navlinks = document.getElementById("navlinks");
+	var menuicon = document.getElementById("icon");
+	if (navlinks.style.display === "block") {
+		navlinks.style.display = "none";
+		menuicon.style.color = "#6cc2fe";
+	} else {
+		navlinks.style.display = "block";
+		menuicon.style.color = "#6cc2fe";
+	}
+}
